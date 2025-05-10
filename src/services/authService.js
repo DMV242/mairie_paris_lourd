c22f531747fb32d6f33a60922ea259fef43dede9
David-Mvoula <david@deplano.fr>
2025-05-10 12:39:54 +0200

    🔧 chore: update package dependencies in package.json and package-lock.json; modify API_URL in authService to point to the new server address

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://YOUR_IP/api';
+const API_URL = 'http://192.168.1.128:8000/api';
 
 // Créer une instance Axios avec des configurations par défaut
 const apiClient = axios.create({
